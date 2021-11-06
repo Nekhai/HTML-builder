73587f09444b9f582982df4bf632300fa5b51399
Nekhai <NekhaiYurii@gmail.com>
2021-11-06 21:41:27 +0200

    feat: add copy-directory task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true}, function(err, items) {
+fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true}, (err, items) => {
   if (err) {
     throw err;
   } 
   items.forEach(item => {
     if (item.isFile()) {
-      // console.log(item.name)
       const fileInfo = path.parse(path.join(__dirname, 'secret-folder', item.name));
-      fs.stat(path.join(__dirname, 'secret-folder', item.name), function(err, stats) {
+      fs.stat(path.join(__dirname, 'secret-folder', item.name), (err, stats) => {
         if (err) {
           throw err;
         } 
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, (err) => {
+  if (err) {
+    throw err;
+  } 
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+    if (err) {
+      throw err;
+    } 
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, items) => {
+    if (err) {
+      throw err;
+    } 
+    items.forEach(item => {
+      fs.copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'files-copy', item.name), (err) => {
+        if (err) {
+          throw err;
+        } 
+      })
+    })
+  });
+});
+
 
